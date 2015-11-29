464bdb431c3f429b7baa7011fd870c0ccd906817
Adriano Iqbal <adrianoiqbal@adrianos-macbook-pro.local>
2015-11-29 10:43:26 -0500

    Initial Commit

---
+++ sadlibs/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sadlibs
+//
+//  Created by Adriano Iqbal on 11/29/15.
+//  Copyright © 2015 Adriano Iqbal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
